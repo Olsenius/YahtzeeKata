e12ca6cfe82a540ec772cf235598785e4af82bbf
Henri Merkesdal <henri.merkesdal@gmail.com>
2010-12-12 13:28:08 +0100

    refaktorert + partester

--- YahtzeeKata/YahtzeeKata/YahtzeeScorer.cs
+++ YahtzeeKata/YahtzeeKata/YahtzeeScorer.cs
@@ -43,5 +43,37 @@ namespace YahtzeeKata
             var numberOfDicesWithMatchingValue = (dices ?? string.Empty).Where(d => Int32.Parse(d.ToString()) == value).Count();
             return numberOfDicesWithMatchingValue * value;
         }
+
+        public static int Score(string dices, ScoreType scoreType)
+        {
+            if (scoreType >= ScoreType.Ones && scoreType <= ScoreType.Sixes)
+            {
+                return SumDicesWithValue(dices, (int)scoreType);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public enum ScoreType
+    {
+        Unknown=0,
+        Ones=1,
+        Twos=2,
+        Threes=3,
+        Fours=4,
+        Fives=5,
+        Sixes=6,
+        Pair=7,
+        TwoPairs=8,
+        ThreeOfAKind=9,
+        FourOfAKind=10,
+        SmallStraight=11,
+        LargeStraight=12,
+        FullHouse=13,
+        Yahtzee=14,
+        Chance=15
     }
 }
