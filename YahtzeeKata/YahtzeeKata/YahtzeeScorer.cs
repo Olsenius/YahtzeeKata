6ad3b85185fd58069b02d5c284da311027aad1e6
Andreas Lien Olsen <andreas.olsen@gmail.com>
2011-02-27 21:14:53 +0100

    Fixed failing test

--- YahtzeeKata/YahtzeeKata/YahtzeeScorer.cs
+++ YahtzeeKata/YahtzeeKata/YahtzeeScorer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace YahtzeeKata
 {
@@ -42,67 +43,19 @@ namespace YahtzeeKata
             return numberOfDicesWithMatchingValue * value;
         }
 
-        public static int ScorePair(string dices)
-        {
-            return ScoreMultiples(dices, 2, 1);
-        }
-
         private static IEnumerable<int> ParseToInt(string dices)
         {
             return (dices ?? String.Empty).Select(x => Int32.Parse(x.ToString()));
         }
 
+        public static int ScorePair(string dices)
+        {
+            return ScoreMultiples(dices, 2, 1);
+        }
 
-        public static object ScoreTwoPairs(string dices)
+        public static int ScoreTwoPairs(string dices)
         {
             return ScoreMultiples(dices, 2, 2);
-        }
-
-        private static int ScoreMultiples(string dices, int numberOfSameDices, int repititions)
-        {
-            var same = dices.GroupBy(x => x)
-                .Where(t => t.Count() >= numberOfSameDices)
-                .OrderByDescending(u => u.Key)
-                .Take(repititions);
-
-            if (same.Count() != repititions)
-                return 0;
-
-            return same.Sum(x => Int32.Parse(x.Key.ToString()) * numberOfSameDices);
-        }
-
-        private static int ScoreMultiplesWithOut(string dices, int numberofSameDices, int repetitions)
-        {
-            int sum = 0;
-            string dicesNow = dices;
-            for (int n = 1; n <= repetitions; n++)
-            {
-                string dicesLeft;
-                var result = GetSame(dicesNow, numberofSameDices, out dicesLeft);
-                if (string.IsNullOrEmpty(result)) return 0;
-
-                sum += ParseToInt(result).Sum();
-                dicesNow = dicesLeft;
-
-            }
-            return sum;
-        }
-
-        private static string GetSame(string dices, int numberOfSame, out string dicesleft)
-        {
-            dicesleft = dices;
-            var same = dices.GroupBy(x => x)
-                .Where(t => t.Count() >= numberOfSame)
-                .OrderByDescending(u => u.Key)
-                .FirstOrDefault();
-
-            if (same != null && same.Count() >= numberOfSame)
-            {
-                dicesleft = dices.Remove(dices.IndexOf(same.First()), numberOfSame);
-                return new string(same.Take(numberOfSame).ToArray());
-            }
-            return "";
-
         }
 
         public static int ScoreThreeOfAKind(string dices)
@@ -113,6 +66,58 @@ namespace YahtzeeKata
         public static int ScoreFourOfAKind(string dices)
         {
             return ScoreMultiples(dices, 4, 1);
+        }
+
+
+        private static int ScoreMultiples(string dices, int numberOfSameDices, int repititions)
+        {
+            var sum = 0;
+            var remainingDices = dices;
+
+            for (int i = 0; i < repititions; i++)
+            {
+                if (!HasMultiples(remainingDices, numberOfSameDices))
+                    return 0;
+
+                sum += TakeBestMultiples(remainingDices, numberOfSameDices);
+                remainingDices = RemoveUsedDices(remainingDices, numberOfSameDices);
+            }
+
+            return sum;
+        }
+
+        private static int TakeBestMultiples(string remainingDices, int numberOfSameDices)
+        {
+            return BestMultipleDice(remainingDices, numberOfSameDices)
+                .Sum(x => Convert.ToInt32(x.ToString()) * numberOfSameDices);
+        }
+
+        private static string RemoveUsedDices(string dices, int numberOfSameDices)
+        {
+            var remainingDices = dices;
+            var bestdice = BestMultipleDice(dices, numberOfSameDices);
+            for (int i = 0; i < numberOfSameDices; i++)
+            {
+                var sb = new StringBuilder(remainingDices);
+                sb.Remove(remainingDices.IndexOf(bestdice), 1);
+                remainingDices = sb.ToString();
+            }
+            return remainingDices;
+        }
+
+        private static string BestMultipleDice(string dices, int numberOfSameDices)
+        {
+            return dices.GroupBy(x => x)
+                .Where(t => t.Count() >= numberOfSameDices)
+                .OrderByDescending(u => u.Key)
+                .FirstOrDefault()
+                .Key
+                .ToString();
+        }
+
+        private static bool HasMultiples(string dices, int numberOfSameDices)
+        {
+            return dices.GroupBy(x => x).Any(x => x.Count() >= numberOfSameDices);
         }
     }
 }
